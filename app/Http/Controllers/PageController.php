b3a2807abd25d538e25cfd158db9d282721b3c9c
luissilva1017 <silvafire2004@gmail.com>
2023-11-15 12:18:38 +0000

    page controller

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        return view('index');
+    }
+
+    public function planostreino(){
+        return view('planostreino');
+    }
+
+    public function blog(){
+        return view('blog');
+    }
+
+    public function modalidades(){
+        return view('modalidades');
+    }
+
+    public function servicos(){
+        return view('servicos');
+    }
+
+    public function contactos(){
+        return view('contactos');
+    }
+}
