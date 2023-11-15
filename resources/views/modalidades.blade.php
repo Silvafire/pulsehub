13394aa208bcf778c1534d4794d78a2b811f469f
luissilva1017 <silvafire2004@gmail.com>
2023-11-15 14:49:35 +0000

    modalidades update

--- resources/views/modalidades.blade.php
+++ resources/views/modalidades.blade.php
@@ -1,16 +1,26 @@
 @extends('layout.master')
 
 @section('moreCSS')
-    <link rel="stylesheet" href="...">
-    
+    <link rel="stylesheet" href="{{ asset('css/modalidades.css') }}">
 @endsection
 
-@section('title','......')
+@section('title', 'Modalidades')
 
 @section('content')
-Conteúdo da página
+    <div class="banner">
+        <img class="darken" src="{{ asset('img/fundo_modalidades.jpg') }}" alt="fundo de modalidades">
+        <div class="middle-title">
+            <h1>Modalidades</h1>
+        </div>
+        <div class="button-container">
+            <a href="#" class="custom-button">
+                <img src="{{ asset('img/icons8-down-button-50.png') }}" alt="Ícone do Botão">
+            </a>
+        </div>
+    </div>
+
 @endsection
 
 @section('moreScripts')
-<!-- opcional -->
+    <!-- opcional -->
 @endsection
