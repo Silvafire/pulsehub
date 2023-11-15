a30489c25eb9d7f3415ee43d6732c0b0b9cd34a7
Tiago Gomes <tiagozne@gmail.com>
2023-11-15 23:28:16 +0000

    nav.blade atualizada
    --> só falta adicionar a route da loja e a página eventos

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -1,44 +1,46 @@
 <nav class="navbar">
-            <div class="container-nav">
+    <div class="container-nav">
+        <div class="logo">
+            <img src="{{asset('img/Logo-PulseHub-Branco.png')}}" alt="logo website branco">
+
+
+        </div>
+        <div class="toggle_btn"><i class="fa-solid fa-bars"></i></div>
+        <div class="all-list">
+            <ul class="list">
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='staff') active @endif" href="{{ route('staff') }}">Staff</a></li>
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='blog') active @endif" href="{{ route('blog') }}">Blog</a></li>
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='planostreino') active @endif" href="{{ route('planostreino') }}">Planos</a></li>
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='modalidades') active @endif" href="{{ route('modalidades') }}">Modalidades</a></li>
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='eventos') active @endif" href="{{ route('eventos') }}">Eventos</a></li>
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='servicos') active @endif" href="{{ route('servicos') }}">Serviços</a></li>
+                <li><a href="#">Loja</a></li>
+            </ul>
+
+        </div>
+        <a href="#" class="button-nav">Login</a>
+
+
+        <div class="dropdown_menu open">
+            <div class="subnav">
                 <div class="logo">
                     <img src="{{asset('img/Logo-PulseHub-Branco.png')}}" alt="logo website branco">
-
-
                 </div>
-                <div class="toggle_btn"><i class="fa-solid fa-bars"></i></div>
-                <div class="all-list">
-                    <ul class="list">
-                        <li><a href="#">Staff</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Planos</a></li>
-                        <li><a class="nav-link scrollto @if (Route::currentRouteName()=='modalidades') active @endif" href="{{ route('modalidades') }}">Modalidades</a></li>
-                        <li><a href="#">Eventos</a></li>
-                        <li><a href="#">Loja</a></li>
-
-                    </ul>
-
-                </div>
-                <a href="#" class="button-nav">Login</a>
-
-
-                <div class="dropdown_menu open">
-                    <div class="subnav">
-                        <div class="logo">
-                            <img src="{{asset('img/Logo-PulseHub-Branco.png')}}" alt="logo website branco">
-                        </div>
-                        <div class="close_btn"><i class="fa-solid fa-xmark"></i></div>
-                    </div>
-
-                    <ul class="list">
-                        <li><a href="#">Staff</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Planos</a></li>
-                        <li><a class="nav-link scrollto @if (Route::currentRouteName()=='modalidades') active @endif" href="{{ route('modalidades') }}">Modalidades</a></li>
-                        <li><a href="#">Eventos</a></li>
-                        <li><a href="#">Loja</a></li>
-
-                    </ul>
-                </div>
+                <div class="close_btn"><i class="fa-solid fa-xmark"></i></div>
             </div>
 
-        </nav>
+            <ul class="list">
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='staff') active @endif" href="{{ route('staff') }}">Staff</a></li>
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='blog') active @endif" href="{{ route('blog') }}">Blog</a></li>
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='planostreino') active @endif" href="{{ route('planostreino') }}">Planos</a></li>
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='modalidades') active @endif" href="{{ route('modalidades') }}">Modalidades</a></li>
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='eventos') active @endif" href="{{ route('eventos') }}">Eventos</a></li>
+                <li><a class="nav-link scrollto @if (Route::currentRouteName()=='servicos') active @endif" href="{{ route('servicos') }}">Serviços</a></li>
+                <li><a href="#">Loja</a></li>
+
+
+            </ul>
+        </div>
+    </div>
+
+</nav>
