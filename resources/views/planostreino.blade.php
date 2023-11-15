4c14bdae49a5579e426f6c8c26e1e82f032ebc8d
Ins1der2317 <150137722+Ins1der2317@users.noreply.github.com>
2023-11-15 17:41:50 +0000

    a

---
+++ resources/views/planostreino.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.master')
+
+@section('moreCSS')
+    <link rel="stylesheet" href="{{ asset('css/PlanosTreino.css') }}">
+@endsection
+
+@section('title', 'PlanosTreino')
+
+@section('content')
