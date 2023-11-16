3192e4aa1656f648f86151335f3994beeeaf0c9d
Tiago Gomes <tiagozne@gmail.com>
2023-11-16 22:30:26 +0000

    pag servicos.blade

--- resources/views/servicos.blade.php
+++ resources/views/servicos.blade.php
@@ -11,6 +11,7 @@
 <!-- Hero -->
 <img class="p-5 text-center bg-image" src="{{ asset('img/bannerservicos.jpg') }}" style="
         background-size: cover;
+        object-fit: cover;
         height: 700px;
         width: 100%;
       ">
@@ -24,14 +25,11 @@
                 <img src="{{asset('img/servicos (3).png')}}" alt="#">
             </div>
             <div class="servicos-tag">
-                <h5>Sandra Silva</h5>
-                <p>Nutricionista</p>
+                <h5>AVALIAÇÃO FÍSICA</h5>
+                <p>Proporcionamos uma avaliação física inicial gratuita com a inscrição.</p>
             </div>
-            <div class="social-media">
-                <a href="#"><img src="{{asset('img/email.svg')}}" alt="email icon"></a>
-                <a href="#"><img src="{{asset('img/insta.svg')}}" alt="instagram icon"></a>
-                <a href="#"><img src="{{asset('img/facebook.svg')}}" alt="facebook icon"></a>
-            </div>
+            <input type="button" value="Ver Mais" class="btn-1"></input>
+
         </div>
 
         <div class="servicos-card">
@@ -39,14 +37,10 @@
                 <img src="{{asset('img/servicos (2).png')}}" alt="#">
             </div>
             <div class="servicos-tag">
-                <h5>Sandra Silva</h5>
-                <p>Nutricionista</p>
+                <h5>GESTOR DE TREINO</h5>
+                <p>Gere o teu treino com a nossa aplicação para o telemóvel.</p>
             </div>
-            <div class="social-media">
-                <a href="#"><img src="{{asset('img/email.svg')}}" alt="email icon"></a>
-                <a href="#"><img src="{{asset('img/insta.svg')}}" alt="instagram icon"></a>
-                <a href="#"><img src="{{asset('img/facebook.svg')}}" alt="facebook icon"></a>
-            </div>
+            <input type="button" value="Ver Mais" class="btn-1"></input>
         </div>
 
         <div class="servicos-card">
@@ -54,14 +48,56 @@
                 <img src="{{asset('img/servicos (1).png')}}" alt="#">
             </div>
             <div class="servicos-tag">
-                <h5>Sandra Silva</h5>
-                <p>Nutricionista</p>
+                <h5>AULAS DE GRUPO</h5>
+                <p>Aulas de grupo diversas, caso estejas a precisar de um levantamento de energias.</p>
             </div>
-            <div class="social-media">
-                <a href="#"><img src="{{asset('img/email.svg')}}" alt="email icon"></a>
-                <a href="#"><img src="{{asset('img/insta.svg')}}" alt="instagram icon"></a>
-                <a href="#"><img src="{{asset('img/facebook.svg')}}" alt="facebook icon"></a>
+            <input type="button" value="Ver Mais" class="btn-1"></input>
+        </div>
+    </div>
+
+    <img class="p-5 text-center bg-image" src="{{ asset('img/dividerservicos.jpg') }}" style="
+        background-size: cover;
+        object-fit: cover;
+        height: 600px;
+        width: 100%;
+        padding-top: 50px;
+        padding-bottom: 50px;
+      ">
+    </img>
+
+
+    <div class="container-grid">
+        <div class="servicos-card">
+            <div class="img-card">
+                <img src="{{asset('img/servicos (6).png')}}" alt="#">
             </div>
+            <div class="servicos-tag">
+                <h5>ACOMPANHAMENTO NUTRICIONAL</h5>
+                <p>Acompanhamento nutricional mensal, com a primeira consulta grátis com a inscrição.</p>
+            </div>
+            <input type="button" value="Ver Mais" class="btn-1"></input>
+        </div>
+
+        <div class="servicos-card">
+            <div class="img-card">
+                <img src="{{asset('img/servicos (5).png')}}" alt="#">
+            </div>
+            <div class="servicos-tag">
+                <h5>TREINO PERSONALIZADO</h5>
+                <p>Personaliza o teu treino à tua maneira. Oferecemos também várias escolhas predefinidas</p>
+            </div>
+            <input type="button" value="Ver Mais" class="btn-1"></input>
+        </div>
+
+        <div class="servicos-card">
+            <div class="img-card">
+                <img src="{{asset('img/servicos (4).png')}}" alt="#">
+            </div>
+            <div class="servicos-tag">
+                <h5>TESTES DE DESEMPENHO</h5>
+                <p>Realiza avaliações físicas iniciais e periódicas para rastreares o teu progresso.</p>
+            </div>
+            <input type="button" value="Ver Mais" class="btn-1"></input>
         </div>
     </div>
 </main>
