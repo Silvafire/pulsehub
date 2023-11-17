33bf10336a982163193d53983517d9a34434f416
luissilva1017 <silvafire2004@gmail.com>
2023-11-17 10:26:13 +0000

    a

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,9 +8,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/navbar.css')}}">
-    <link rel="stylesheet" href="{{asset('css/footer.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
     @yield('moreCSS')
     <title> @yield('title')</title>
 </head>
@@ -22,8 +23,8 @@
     @yield('content')
 
     @include('layout.partials.footer')
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('js/navbar.js')}}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/navbar.js') }}"></script>
 
     @yield('moreScripts')
 
