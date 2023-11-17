48408ad3c87fe7c31fed9c0ee99ee30ed714edda
luissilva1017 <silvafire2004@gmail.com>
2023-11-17 10:14:05 +0000

    add bootstrap

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{asset('css/bootsrap.mim.css')}}">
     <link rel="stylesheet" href="{{asset('css/navbar.css')}}">
     <link rel="stylesheet" href="{{asset('css/footer.css')}}">
     @yield('moreCSS')
@@ -21,7 +22,7 @@
     @yield('content')
 
     @include('layout.partials.footer')
-
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
     <script src="{{asset('js/navbar.js')}}"></script>
 
     @yield('moreScripts')
