92f94a5da9f5839b11ca6830c2069ac16f12fcf5
AndreSantos-2460 <santossteamcs@gmail.com>
2023-11-27 14:53:47 +0000

    tabela staff criada

---
+++ database/migrations/2023_11_27_142700_create_staff_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome',45);
+            $table->string('apelido',50);
+            $table->string('Especialização',45);
+            $table->string('link_facebook', 200);
+            $table->string('link_instagram', 200);
+            $table->string('link_email', 200);
+            $table->string('img');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('staff');
+    }
+};
