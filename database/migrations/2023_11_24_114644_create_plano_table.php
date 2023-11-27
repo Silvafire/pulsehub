994a33bf0afe0867003df887d253a9da3db970e4
Ins1der2317 <150137722+Ins1der2317@users.noreply.github.com>
2023-11-27 15:26:35 +0000

    atualiza primo

--- database/migrations/2023_11_24_114644_create_plano_table.php
+++ database/migrations/2023_11_24_114644_create_plano_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('plano', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->num('Series');
+            $table->time('Duracao_Total');
+            $table->time('Tmp_Exercicio');
+            $table->num('Repeticoes');
+            $table->unsignedBigInteger('modalidades_id'); // Chave estrangeira
+            $table->foreign('modalidades_id')->references('id')->on('modalidades');
+            $table->softDeletes(); // Adiciona a coluna deleted_at para soft deletes
         });
     }
 
