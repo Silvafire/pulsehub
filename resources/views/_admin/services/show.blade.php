6285b6a3d84c51a58caf8f0b31ae0c581052cd4c
Tiago Gomes <tiagozne@gmail.com>
2023-11-28 21:10:07 +0000

    servicos modal
    com bd a utilizar

--- resources/views/_admin/services/show.blade.php
+++ resources/views/_admin/services/show.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container-fluid">
 
-     <div class="card shadow mb-4">
-        <div class="card-header py-3">
+	<div class="card shadow mb-4">
+		<div class="card-header py-3">
 			Informação do Serviço
-        </div>
-        <div class="card-body">
+		</div>
+		<div class="card-body">
 
 			<div><strong>Nome:</strong>{{$service->nome}}</div>
 			<div><strong>Descrição:</strong>{{$service->descricao}}</div>
