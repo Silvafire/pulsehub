c3e8bbb9a79e83e87cb830e50fa67a210cfabb33
luissilva1017 <silvafire2004@gmail.com>
2023-12-04 15:30:26 +0000

    add modalidades admin

---
+++ resources/views/_admin/modalidades/partials/add-edit.blade.php
@@ -0,0 +1,9 @@
+<div class="form-group">
+    <label for="inputName">Nome</label>
+    <input type="text" class="form-control" name="name" id="inputName" value="{{old('name',$modalidades->name)}}" />
+</div>
+
+<div class="form-group">
+    <label for="inputDescription">Descrição</label>
+    <textarea class="form-control" name="description" id="inputDescription"></textarea>
+</div>
