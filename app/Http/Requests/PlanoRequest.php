aa1b733d1e24bd74cb91b6d8e2df058984fc2921
Ins1der2317 <150137722+Ins1der2317@users.noreply.github.com>
2023-12-04 16:29:34 +0000

    planos

---
+++ app/Http/Requests/PlanoRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PlanoRequest extends FormRequest
+{
+    //Determine if the user is authorized to make this request.*/
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /* Get the validation rules that apply to the request.*
+
+@return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>*/
+
+
+    public function rules(): array
+    {
+        $currentId = $this->plano ? $this->plano->id : null;
+        return [
+            "Series" => 'required|integer|min:1', 
+        "Duracao_Total" => 'required|integer|min:1', 
+        "Tmp_Exercicio" => 'required|integer|min:1', 
+        "Repeticoes" => 'required|integer|min:1',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nome.regex' => 'O nome deve conter apenas letras e espaços',
+            'descricao' => 'A descrição deve conter apenas letras e espaços',
+            'img' => 'Imagem demaisado grande',
+            'imagem_id' => 'Tipo de evento não existente',
+        ];
+    }
+}
