4d8e258fc902510c49bbf31bdb2c2aac2b8cf1a2
LAPTOP-SDG51CHG\OMEN <tiagoalexandrequintino@gmail.com>
2023-12-11 15:31:55 +0000

    tipo_evento_mod tiagoQ e Luis

---
+++ app/Http/Requests/Tipo_evento_modRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Tipo_evento_modRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $currentId = $this->tem ? $this->tem->id : null;
+        return [
+            "nome" => 'required|min:3|max:80|unique:events,nome,' .
+                $currentId . '|regex:/^[A-ZÀ-úa-z\s]+$/',
+            'imagem' =>'nullable|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nome.regex' => 'O nome deve conter apenas letras e espaços',
+            'img.max' => 'Imagem demaisado grande',
+        ];
+    }
+}
