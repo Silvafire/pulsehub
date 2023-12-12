300d58ce3623acde1eafe2ebcd239da895a99d80
Ins1der2317 <150137722+Ins1der2317@users.noreply.github.com>
2023-12-12 15:29:20 +0000

    imagem tipoos planos

--- resources/views/_admin/tiposplanos/partials/add-edit.blade.php
+++ resources/views/_admin/tiposplanos/partials/add-edit.blade.php
@@ -9,7 +9,8 @@
 </div>
 
 <div class="form-group">
-    <label for="inputImage">Imagem</label>
+    <label for="inputImage">Imagem</label><br>
+    <img height='80' style="margin-bottom:1rem" src="{{asset('storage/tiposplanos_imagens/'. $tiposplano->imagem )}}" alt="Imagem do tipop">
     <input type="file" class="form-control-file" name="imagem" id="inputImage" aria-describedby="fileHelp" />
     <small id="fileHelp" class="form-text text-muted"> Por favor carregue um ficheiro de imagem válido. O tamanho da imagem não deve exceder 2MB. </small>
 </div>
