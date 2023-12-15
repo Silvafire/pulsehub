a51c0ee2ca60b5332a9f4a8abb74f663fa51591b
luissilva1017 <silvafire2004@gmail.com>
2023-12-15 17:34:44 +0000

    update card

--- resources/views/_admin/dashboard.blade.php
+++ resources/views/_admin/dashboard.blade.php
@@ -16,16 +16,22 @@
                     </div>
                 </div>
                 {{-- <div class="card shadow">
-                    <div class="card-header p-4">
-                        <h1 style="display:inline;"> {{ $count_modalidade }} </h1>
-                        <h3 style="display:inline;"> Modalidades </h3>
-                    </div>
-                    <div class="card-body">
-                        @foreach ($count_modalidades as $categoria)
-                            <p>{{ $categoria->roleToStr() }}: {{ $categoria->count }} utilizadores</p>
-                        @endforeach
-                    </div>
-                </div> --}}
+    <div class="card-header p-4">
+        <h1 style="display:inline;"> {{ $count_modalidade }} </h1>
+        <h3 style="display:inline;"> Modalidades </h3>
+    </div>
+    <div class="card-body">
+        <p>Total de Modalidades: {{ $count_modalidade }}</p>
+        @if (count($count_modalidades) > 0)
+            <p>Modalidades por Categoria:</p>
+            @foreach ($count_modalidades as $categoria)
+                <p>{{ $categoria->roleToStr() }}: {{ $categoria->count }} modalidades</p>
+            @endforeach
+        @else
+            <p>Nenhuma informação de categoria de modalidade disponível.</p>
+        @endif
+    </div>
+</div> --}}
             </div>
         </div>
     </div>
