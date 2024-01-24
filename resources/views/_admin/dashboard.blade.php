049d246430018c7988494516e0abeca6c2e8a5b7
Tiago Gomes <tiagozne@gmail.com>
2024-01-24 22:38:34 +0000

    Image services mudanças

--- resources/views/_admin/dashboard.blade.php
+++ resources/views/_admin/dashboard.blade.php
@@ -37,8 +37,8 @@
                         <h3 style="display:inline;"> Modalidades </h3>
                     </div>
                     <div class="card-body">
-                        @foreach ($count_mod_per_type as $item)
-                            <p>{{ $item->tipo->nome }} : {{ $item->count }} Modalidades</p>
+                        @foreach ($count_services_per_image as $item)
+                            <p>{{ $item->service->nome }} : {{ $item->count }} Imagens</p>
                         @endforeach
                     </div>
                 </div>
@@ -46,12 +46,13 @@
             <div class="col-3">
                 <div class="card shadow">
                     <div class="card-header p-4">
-                        <h1 style="display:inline;"> {{ $services->count() }} </h1>
+                        <h1 style="display:inline;"> {{ $count_services }} </h1>
                         <h3 style="display:inline;"> Serviços </h3>
                     </div>
                     <div class="card-body">
-                        @foreach ($services as $item)
-                            <p>{{ $item->nome }} : {{ $item->images()->count() }} Imagens</p>
+                        @foreach ($count_services_per_image as $item)
+                        <p>{{ $item  }} :{{ $item->count }} Imagens</p>
+
                         @endforeach
                     </div>
                 </div>
