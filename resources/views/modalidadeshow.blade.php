4315d5e853aa27a7983c0c18ef6bcf7f83c5cd52
luissilva1017 <silvafire2004@gmail.com>
2024-01-26 19:39:34 +0000

    update modalidades

--- resources/views/modalidadeshow.blade.php
+++ resources/views/modalidadeshow.blade.php
@@ -7,15 +7,41 @@
 @section('title', 'Modalidade ')
 
 @section('content')
+
+    <div class="image">
+        <img src="{{ asset('storage/modalidades_imagens/' . $modalidade->imagem) }}" alt="Descrição da imagem">
+    </div>
     <div class="modalidades">
         <div class="modalidade">
             <div class="content-1">
-                <div class="image">
-                    <img src="{{ asset('storage/modalidades_imagens/' . $modalidade->imagem) }}" alt="Descrição da imagem">
-                </div>
+
                 <div class="text">
                     <h2>{{ $modalidade->nome }}</h2>
                     <p>{{ $modalidade->descricao }}</p>
+                    <p>{{ $modalidade->tipo->nome }}</p>
+                    @if (count($modalidade->staff))
+                        <div><strong>Staff associado à modalidade: </strong>
+                            <ul>
+                                @foreach ($modalidade->staff as $membro)
+                                    <li>
+                                        {{ $membro->nome . ' ' . $membro->apelido }}
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    @if (count($modalidade->planos))
+                        <div><strong>Planos associado à modalidade: </strong>
+                            <ul>
+                                @foreach ($modalidade->planos as $plano)
+                                    <li>
+                                        {{ $plano->tipo_plano->nome . '-' . $plano->duracao_total }}
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
