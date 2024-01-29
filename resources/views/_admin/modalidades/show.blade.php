ce2122c18db6493b9026aee9761ca8fd76b0cf85
luissilva1017 <silvafire2004@gmail.com>
2024-01-29 15:41:29 +0000

    update show modalidades e tipo modalidades

--- resources/views/_admin/modalidades/show.blade.php
+++ resources/views/_admin/modalidades/show.blade.php
@@ -1,42 +1,68 @@
 @extends('layout.admin')
 
 @section('content')
-<div class="container-fluid">
+    <div class="container-fluid">
 
-	<div class="card shadow mb-4">
-		<div class="card-header py-3">
-			Informação da Modalidade
-		</div>
-		<div class="card-body">
-
-			<div><strong>Nome: </strong>{{$modalidade->nome}}</div>
-            <div><strong>Descrição: </strong>{{$modalidade->descricao}}</div>
-            <div><strong>Imagem: </strong><img height='80' src="{{asset('storage/modalidades_imagens/'. $modalidade->imagem )}}" alt="Imagem da modalidade"></div>
-            <div><strong>Categoria: </strong>{{ $modalidade->tipo->nome}}</div>
-            @if(count($modalidade->staff))
-            <div><strong>Staff associado à modalidade: </strong>
-                <ul>
-                    @foreach($modalidade->staff as $membro)
-                        <li>
-                            {{$membro->nome . ' '. $membro->apelido }}
-                        </li>
-                    @endforeach
-                </ul>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                Informação da Modalidade
             </div>
-            @endif
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-3 mb-3">
+                        <strong>Imagem:</strong>
+                        <img class="img-fluid" src="{{ asset('storage/modalidades_imagens/' . $modalidade->imagem) }}"
+                            alt="Imagem da modalidade">
+                    </div>
 
-            @if(count($modalidade->planos))
-            <div><strong>Planos associado à modalidade: </strong>
-                <ul>
-                    @foreach($modalidade->planos as $plano)
-                        <li>
-                            {{$plano->tipo_plano->nome . '-'. $plano->duracao_total }}
-                        </li>
-                    @endforeach
-                </ul>
+                    <div class="col-md-9">
+                        <div class="mb-3">
+                            <strong>Nome:</strong> {{ $modalidade->nome }}
+                        </div>
+                        <div class="mb-3">
+                            <strong>Categoria:</strong> {{ $modalidade->tipo->nome }}
+                        </div>
+                        <div class="mb-3">
+                            <strong>Descrição:</strong> {{ $modalidade->descricao }}
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        @if (count($modalidade->staff))
+                            <div class="mb-3">
+                                <strong>Staff associado à modalidade:</strong>
+                                <ul class="list-unstyled">
+                                    @foreach ($modalidade->staff as $membro)
+                                        <li>{{ $membro->nome . ' ' . $membro->apelido }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                    </div>
+
+                    <div class="col-md-6">
+                        @if (count($modalidade->planos))
+                            <div class="mb-3">
+                                <strong>Duração dos planos associados à modalidade:</strong>
+                                <ul class="list-unstyled">
+                                    @foreach ($modalidade->planos as $plano)
+                                        <li>{{ $plano->tipo_plano->nome . ' - ' . $plano->duracao_total . ' minutos' }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                    </div>
+                </div>
             </div>
-            @endif
-		</div>
-	</div>
-</div>
+
+
+
+
+
+        </div>
+    </div>
 @endsection
--- resources/views/_admin/tipo_eventos_mod/show.blade.php
+++ resources/views/_admin/tipo_eventos_mod/show.blade.php
@@ -7,11 +7,16 @@
             <div class="card-header py-3">
                 Informação do tipo eventos/modalidades
             </div>
-            <div class="card-body">
-
-                <div><strong>Nome:</strong>{{ $tipo_eventos_mod->nome }}</div>
-                <div><strong>imagem:</strong>{{ $tipo_eventos_mod->imagem }}</div>
-
+            <div class="card">
+                <div class="card-body">
+                    <div>
+                        <strong>Imagem:</strong>
+                        <img src="{{ asset('storage/tipo_modalidades/' . $tipo_eventos_mod->imagem) }}" alt="Imagem da modalidade" style="max-width: 300px;">
+                    </div>
+                    <div class="mr-3">
+                        <strong>Nome:</strong> {{ $tipo_eventos_mod->nome }}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
