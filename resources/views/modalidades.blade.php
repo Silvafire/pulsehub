a7eb963257e7fc0334ddd315e9e485a7eb1ab6e6
luissilva1017 <silvafire2004@gmail.com>
2024-01-29 17:41:49 +0000

    update modalidades

--- resources/views/modalidades.blade.php
+++ resources/views/modalidades.blade.php
@@ -8,26 +8,26 @@
 
 @section('content')
     <div class="banner">
-        <img class="darken" src="{{ asset('img/fundo_modalidades.jpg') }}" alt="fundo de modalidades">
+        <div id="img" class="darken">
+            <img src="{{ asset('img/fundo_modalidades.jpg') }}" alt="fundo de modalidades">
+        </div>
         <div class="middle-title">
             <h1>Modalidades</h1>
         </div>
         <div class="button-container">
-            <a href="#bottom-title" class="custom-button">
+            <a href="#categorias" class="custom-button">
                 <img src="{{ asset('img/icons8-down-button-50.png') }}" alt="Ícone do Botão">
             </a>
         </div>
     </div>
 
 
+
     <div class="container">
-        <div class="bottom-title" id="bottom-title">
-            <h2>Categorias de modalidades</h2>
+        <div class="bottom-title" id="categorias">
+            <h2>Categorias das modalidades</h2>
             <!-- Carrossel de cards -->
-
             <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-
-
                 <div class="carousel-inner">
                     @foreach ($tipo_eventos_mods->chunk(3) as $key => $chunk)
                         <div class="carousel-item{{ $key == 0 ? ' active' : '' }}">
@@ -56,7 +56,8 @@
                 <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
                     data-bs-slide="next">
                     <img src="{{ asset('img/icons8-arrow-100-r.png') }}" alt="Próximo">
-                </button></div>
+                </button>
+            </div>
         </div>
     </div>
 
@@ -65,7 +66,7 @@
         <div class="modalidade">
             @if ($modalidades && count($modalidades))
                 @foreach ($modalidades as $key => $modalidade)
-                    <div class="content-{{ $key % 2 + 1 }}">
+                    <div class="content-{{ ($key % 2) + 1 }}">
                         <div class="image">
                             <img src="{{ asset('storage/modalidades_imagens/' . $modalidade->imagem) }}"
                                 alt="Descrição da imagem">
