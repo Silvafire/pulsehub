7af628a42895bf60ab4dad8e8c66f43566485bac
luissilva1017 <silvafire2004@gmail.com>
2024-01-30 15:58:22 +0000

    update footer

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -3,13 +3,19 @@
         <div class="upper-footer">
             <div class="logo"><img src="{{ asset('img/Logo-PulseHub-Branco.png') }}" alt="logo branco pulse hub"></div>
             <ul class="list-footer">
-                <li><a href="#">Staff</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Planos</a></li>
+                <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'staff') active @endif"
+                        href="{{ route('staff') }}">Staff</a></li>
+                <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'blog') active @endif"
+                        href="{{ route('blog') }}">Blog</a></li>
+                <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'planostreino') active @endif"
+                        href="{{ route('planostreino') }}">Planos</a></li>
                 <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'modalidades') active @endif"
                         href="{{ route('modalidades') }}">Modalidades</a></li>
-                <li><a href="#">Eventos</a></li>
-                <li><a href="#">Loja</a></li>
+                <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'eventos') active @endif"
+                        href="{{ route('eventos') }}">Eventos</a></li>
+                <li><a class="nav-hiper scrollto @if (Route::currentRouteName() == 'servicos') active @endif"
+                        href="{{ route('servicos') }}">Serviços</a></li>
+                <li><a href="https://wordpress.g3.dwm2023.fun/">Loja</a></li>
 
             </ul>
             <div class="social-footer">
